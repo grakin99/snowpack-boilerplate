1962941b88732967369808c30a1d86cfa1532e76
Graham Kineshanko <grahamkineshanko@gmail.com>
2021-02-16 17:08:31 -0700

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,23 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+    mount: {
+        src: "/",
+    },
+    plugins: [
+        /* ... */
+    ],
+    packageOptions: {
+        /* ... */
+    },
+    devOptions: {
+        /* ... */
+    },
+    buildOptions: {
+        /* ... */
+    },
+    optimize: { bundle: true, minify: true, target: "es2020" },
+    exclude: ["**/node_modules/**", "**/scss/**"],
+};
---
+++ src/js/index.js
@@ -0,0 +1,2 @@
+import { helloWorld } from "./log";
+helloWorld();
